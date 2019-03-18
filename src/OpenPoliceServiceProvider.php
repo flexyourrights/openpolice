2ceda7e086e8f724a44056c333cfc3dbaf43126d
Morgan Lesko <wikiworldorder@protonmail.com>
2019-03-18 10:39:58 -0400

    Reorganizing Install Process

--- src/OpenPoliceServiceProvider.php
+++ src/OpenPoliceServiceProvider.php
@@ -5,6 +5,19 @@ use Illuminate\Support\ServiceProvider;
 
 class OpenPoliceServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+        /*
+        * Register the service provider for the dependency.
+        */
+        $this->app->register('OpenPolice\OpenPoliceServiceProvider');
+        /*
+        * Create aliases for the dependency.
+        */
+        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        $loader->alias('OpenPolice', 'FlexYourRights\OpenPolice\OpenPoliceFacade');
+    }
+
     public function boot()
     {
         require __DIR__ . '/routes.php';
@@ -14,10 +27,15 @@ class OpenPoliceServiceProvider extends ServiceProvider
             __DIR__.'/Models'  => base_path('app/Models/OpenPolice'),
             __DIR__.'/Public'  => base_path('public/openpolice'),
             __DIR__.'/Uploads' => base_path('storage/app/up/openpolice'),
+
             __DIR__.'/Database/2018_12_07_OP_create_tables.php'
                 => base_path('database/migrations/2018_12_07_OP_create_tables.php'),
-            __DIR__.'/Database/OpenPoliceSeeder.php' => base_path('database/seeds/OpenPoliceSeeder.php'),
-            __DIR__.'/Database/OpenPoliceDeptSeeder.php' => base_path('database/seeds/OpenPoliceDeptSeeder.php'),
+            __DIR__.'/Database/OpenPoliceSeeder.php'
+                => base_path('database/seeds/OpenPoliceSeeder.php'),
+            __DIR__.'/Database/OpenPoliceDeptSeeder.php'
+                => base_path('database/seeds/OpenPoliceDeptSeeder.php'),
+            base_path('/vendor/flexyourrights/openpolice-departments/src/OpenPoliceDeptSeeder.php')
+                => base_path('database/seeds/OpenPoliceDeptSeeder.php')
         ]);
     }
 }
